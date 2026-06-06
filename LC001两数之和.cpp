a17bf1ca7836d3c1d2f8c18b5607f4a64b64c302
EPxiaoma <599902085@qq.com>
2026-06-06 10:54:25 +0800

    LC001两数之和

---
+++ LC001两数之和.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 核心代码
+class Solution {
+public:
+    vector<int> solve(vector<int> &nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int num = target - nums[i];
+            if (map.find(num) != map.end()) {
+                return {i, map[num]};
+            } else {
+                map[nums[i]] = i;
+            }
+        }
+
+        return {};
+    }
+};
+
+// 输入 → 调用 Solution → 输出
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = Solution().solve(nums, target);
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+}
