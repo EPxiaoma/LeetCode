73ee3cf163bf1092e9a0dd9a6b47a77a4e54985e
EPxiaoma <599902085@qq.com>
2026-06-09 09:53:50 +0800

    LC033_搜索旋转排序数组

---
+++ LC033_搜索旋转排序数组.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================================================================
+// 核心代码
+// =====================================================================
+class Solution {
+public:
+    int solve(vector<int> nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+// =====================================================================
+// 主函数: 输入 → 调用 Solution → 输出
+// =====================================================================
+int main() {
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+    int result = Solution().solve(nums, target);
+    cout << result << endl;
+}
