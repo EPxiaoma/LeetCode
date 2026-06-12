fca207bd83a738e18c12e98a52b8f958418dbb87
EPxiaoma <599902085@qq.com>
2026-06-12 11:13:11 +0800

    LC057_插入区间

---
+++ LC057_插入区间.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================================================================
+// 核心代码
+// =====================================================================
+class Solution {
+public:
+    static bool cmp(vector<int> &a, vector<int> &b) {
+        return a[0] < b[0];
+    }
+
+    vector<vector<int> > solve(vector<vector<int> > &intervals, vector<int> &newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end(), cmp);
+        vector<vector<int> > result;
+        result.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] <= result.back()[1]) {
+                result.back()[1] = max(intervals[i][1], result.back()[1]);
+            } else {
+                result.push_back(intervals[i]);
+            }
+        }
+
+        return result;
+    }
+};
+
+// =====================================================================
+// 主函数: 输入 → 调用 Solution → 输出
+// =====================================================================
+int main() {
+    vector<vector<int> > intervals = {
+        {1, 3},
+        {6, 9},
+    };
+
+    vector<int> newInterval = {2, 5};
+
+    vector<vector<int> > result = Solution().solve(intervals, newInterval);
+    cout << '[';
+    for (int i = 0; i < result.size(); i++) {
+        cout << '[';
+        for (int j = 0; j < result[i].size(); j++) {
+            cout << result[i][j];
+            if (j < result[i].size() - 1) {
+                cout << ',';
+            }
+        }
+        cout << ']';
+        if (i < result.size() - 1) {
+            cout << ',';
+        }
+    }
+    cout << ']';
+}
