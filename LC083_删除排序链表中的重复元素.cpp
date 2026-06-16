6c9d3b9523d907a242186921d1ff197e009d33c9
EPxiaoma <599902085@qq.com>
2026-06-16 10:15:27 +0800

    LC083_删除排序链表中的重复元素

---
+++ LC083_删除排序链表中的重复元素.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// =====================================================================
+// 链表
+// =====================================================================
+
+// 链表数据结构
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {
+    }
+
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {
+    }
+};
+
+// 从 vector 构建链表
+ListNode *buildList(const vector<int> &nums) {
+    ListNode *dummy = new ListNode();
+    ListNode *cur = dummy;
+    for (int v: nums) {
+        cur->next = new ListNode(v);
+        cur = cur->next;
+    }
+    return dummy->next;
+}
+
+// 打印链表
+void printList(ListNode *head) {
+    cout << "[";
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << ",";
+        head = head->next;
+    }
+    cout << "]" << endl;
+}
+
+// =====================================================================
+// 核心代码
+// =====================================================================
+class Solution {
+public:
+    ListNode *solve(ListNode *head) {
+        ListNode *cur = head;
+
+        while (cur && cur->next) {
+            if (cur->val == cur->next->val) {
+                cur->next = cur->next->next;
+            } else {
+                cur = cur -> next;
+            }
+        }
+
+        return head;
+    }
+};
+
+// =====================================================================
+// 主函数: 输入 → 调用 Solution → 输出
+// =====================================================================
+int main() {
+    vector<int> v = {1, 1, 2};
+    ListNode *head = buildList(v);
+    ListNode *result = Solution().solve(head);
+    printList(result);
+}
